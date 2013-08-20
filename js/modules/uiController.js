f730f1d65229596390c5c859991765a493371f6a
Andre Silva <andre@bynd.com>
2013-08-20 11:05:09 +0100

    Added comments to uiController module

--- js/modules/uiController.js
+++ js/modules/uiController.js
@@ -4,7 +4,7 @@
 
 define(["jquery", "use!underscore", "coinCounter"], function ($, _, coinCounter) {
 
-	// Private @ Reference to each DOM element
+	// Private @ Reference to each DOM element wrapped in a jQuery object
 	var elements = {
 		theForm: {
 			theForm: $(".js-form"),
@@ -20,22 +20,24 @@ define(["jquery", "use!underscore", "coinCounter"], function ($, _, coinCounter)
 		}
 	};
 
-	// Private @ Currency symbol
-	var currency = "£";
-
-	// Private @ Contents of each template
+	// Private @ Unparsed contents of each HTML template
 	var templates = {
 		coins: $(".js-template-coin").html()
 	};
 
-	// Public @ Initialise the form
+	// Private @ Currency symbol to be appended to strings
+	var currency = "£";
+
+	// Public @ Add basic form validation and handle submissions
 	var initialiseForm = function () {
 		elements.theForm.theForm.on("submit", function () {
 			var value = elements.theForm.amount.val();
 			if (value.length < 1) {
+				// Input is Valid
 				elements.theForm.message.show();
 				elements.results.wrapper.hide();
 			} else {
+				// Input is Invalid
 				requestCalculations(value);
 				elements.theForm.message.hide();
 			}
@@ -43,40 +45,42 @@ define(["jquery", "use!underscore", "coinCounter"], function ($, _, coinCounter)
 		});
 	};
 	
+	// Private @ Request calculation from the module and check for type of response received
 	var requestCalculations = function (value) {
 		var coinCalculation = coinCounter.calculateCoins(value);
 		if (coinCalculation) {
+			// Response is Object - OK
 			updateResults(coinCalculation);
 			elements.results.wrapper.show();
 		} else {
+			// Response is False - Error
 			elements.theForm.message.show();
 			elements.results.wrapper.hide();
 		};
 	};
 
+	// Private @ Update DOM after results have been received
 	var updateResults = function (results) {
-		console.log("Showing Results...", results);
 		
 		// Update Summary
-		elements.results.theValue.html(results.value);
+		elements.results.theValue.html(currency + results.value);
 		elements.results.coins.html(results.sumCoins);
 
-		// Reset Coin Container
-		elements.results.container.html("");
+		// Update Coins
+		var output = "", 
+			contents;
 
-		var output = "";
 		for (coin in results.allCoins) {
 			var contents = _.template(templates.coins, results.allCoins[coin]);
 			output += contents;
 		}
 
+		// Write coins to container
 		elements.results.container.html(output);
 
 	};
 
-	
-
-	// Expose public variables and functions to be accessible outside of the module
+	// Expose public variables to be accessible outside the module
     return {
         initialiseForm: initialiseForm
     };
